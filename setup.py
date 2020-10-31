acb6522f62c27a720ac8219062d59bf68b9594e8
Imran Qureshi <imranq2@hotmail.com>
2020-10-30 21:25:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -80,6 +80,6 @@ setup(
     dependency_links=[],
     include_package_data=True,
     zip_safe=False,
-    package_data={package_name: ["py.typed"]},
+    package_data={"spark_fhir_schemas": ["py.typed"]},
     data_files=[]
 )
